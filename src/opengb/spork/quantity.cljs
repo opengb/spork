271417434b3b14d8c750fecadb22c7c036211c8e
Ryan McCuaig <ryan@ryanmccuaig.net>
2019-12-11 11:48:07 -0800

    Moves unit->ui-string to dram lib

--- src/opengb/spork/quantity.cljs
+++ src/opengb/spork/quantity.cljs
@@ -1,6 +1,7 @@
 (ns opengb.spork.quantity
   (:require
    [goog.i18n.NumberFormat.Format]
+   [opengb.dram.quantity]
    [reagent.core :as reagent]
    [stylefy.core :refer [use-style]])
   (:import (goog.i18n NumberFormat)
@@ -23,30 +24,12 @@
   [n]
   [:span.mag [:strong (format-as-number n)]])
 
-(defn unit->ui-string
-  "Formats the `unit` for display to a user."
-  [unit]
-  (let [cs {:mi2            "sq mi"
-            :ft2            "sq ft"
-            :ft2_a          "ft²/year"
-            :USD_ft2_a      "USD/ft²/year"
-            :m2             "m²"
-            :GJ_m2_a        "GJ/m²/year"
-            :kWh_m2_a       "kWh/m²/year"
-            :tCO2e_a        "tCO₂e/year"
-            "m**2"          "m²"
-            "kWh/m**2/year" "kWh/m²"
-            "kg/m**2/year"  "kg/m²"
-            "l/m**2/year"   "l/m²"
-            }]
-    (get cs unit unit)))
-
 (defn Unit
   [k]
   [:span.unit (use-style {:margin-left    "0.25em"
                           :text-transform "none !important"
                           :opacity        0.4})
-   (unit->ui-string k)])
+   (opengb.dram.quantity/unit->ui-string k)])
 
 (defn Quantity
   [[mag unit]]
