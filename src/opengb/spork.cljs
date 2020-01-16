56ff2e619a5dbe174c228c559150e5367464afbe
Ryan McCuaig <ryan@ryanmccuaig.net>
2020-01-16 10:23:47 -0800

    Tidy up public api namespace

--- src/opengb/spork.cljs
+++ src/opengb/spork.cljs
@@ -1,52 +1,30 @@
 (ns opengb.spork
+  "Reagent/Re-Frame view component library."
   (:require
-   [opengb.spork.design-note :as design-note]
-   [opengb.spork.error-boundary :as error-boundary]
+   [opengb.spork.design-note       :as design-note]
+   [opengb.spork.error-boundary    :as error-boundary]
    [opengb.spork.keyboard-listener :as kb]
-   [opengb.spork.leaflet :as leaflet]
-   [opengb.spork.markdown :as markdown]
-   [opengb.spork.quantity :as qty]
-   [opengb.spork.vega :as vega]
-   [opengb.spork.virtualized :as virtualized]))
+   [opengb.spork.leaflet           :as leaflet]
+   [opengb.spork.markdown          :as markdown]
+   [opengb.spork.quantity          :as qty]
+   [opengb.spork.vega              :as vega]
+   [opengb.spork.virtualized       :as virtualized]))
 
-(def ^:deprecated Map leaflet/CircleMarkerMap)
+;; * Public API facades
 
-(def CircleMarkerMap leaflet/CircleMarkerMap)
-
-(def Chart vega/Chart)
-
-(def AutoSizer virtualized/AutoSizer)
-
-(def StretchHeight virtualized/AutoHeighter)
-
-(def StretchWidth virtualized/AutoWidther)
-
+(def AutoSizer          virtualized/AutoSizer)
+(def Chart              vega/Chart)
+(def CircleMarkerMap    leaflet/CircleMarkerMap)
+(def DesignNote         design-note/DesignNote)
+(def ErrorBoundary      error-boundary/ErrorBoundary)
+(def KeyboardListener   kb/KeyboardListener)
+(def Markdown->Hiccup   markdown/Markdown->Hiccup)
+(def Quantity           qty/Quantity)
+(def StretchHeight      virtualized/AutoHeighter)
 (def StretchHeightWidth virtualized/AutoSizer2)
+(def StretchWidth       virtualized/AutoWidther)
 
-(def KeyboardListener kb/KeyboardListener)
-
-(def Quantity qty/Quantity)
-
-(def DesignNote design-note/DesignNote)
-
-(def ^:deprecated init-design-notes
-  "[]
-   Enables re-frame-driven showing/hiding of design notes.
-   Provides these re-frame events:
-
-   `[:opengb.spork.design-note/show]`
-   `[:opengb.spork.design-note/hide]`
-   `[:opengb.spork.design-note/toggle]`
-
-   Provides this re-frame subscription:
-   `[:opengb.spork.design-note/showing?]`"
-  design-note/register-re-frame-handlers)
-
-(def ^:deprecated Markdown markdown/Markdown->Hiccup)
-
-(def Markdown->Hiccup markdown/Markdown->Hiccup)
-
-(def ErrorBoundary error-boundary/ErrorBoundary)
+;; * re-frame handler registration
 
 (def ^:private get-rf-initializer
   {:design-notes design-note/register-re-frame-handlers
@@ -64,3 +42,22 @@
    (doseq [k (filter valid-key? component-keys)
            :let [handler (get-rf-initializer k)]]
      (handler)))))
+
+;; * Old vars; don't use these any more
+
+(def ^:deprecated Map leaflet/CircleMarkerMap)
+
+(def ^:deprecated Markdown markdown/Markdown->Hiccup)
+
+(def ^:deprecated init-design-notes
+  "[]
+   Enables re-frame-driven showing/hiding of design notes.
+   Provides these re-frame events:
+
+   `[:opengb.spork.design-note/show]`
+   `[:opengb.spork.design-note/hide]`
+   `[:opengb.spork.design-note/toggle]`
+
+   Provides this re-frame subscription:
+   `[:opengb.spork.design-note/showing?]`"
+  design-note/register-re-frame-handlers)
